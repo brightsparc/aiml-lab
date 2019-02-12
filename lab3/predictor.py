8b4e643749f5325b925157b95c6661957c926fc3
Julian Bright <brightsparc@gmail.com>
2019-02-13 08:45:00 +1100

    Adding initial sagemaker inference endpoint

---
+++ lab3/predictor.py
@@ -0,0 +1,76 @@
+# This is the file that implements a flask server to do inferences. It's the file that you will modify to
+# implement the scoring for your own algorithm.
+
+from __future__ import print_function
+
+import os
+import json
+import pickle
+import StringIO
+import sys
+import signal
+import traceback
+
+import flask
+
+import pandas as pd
+
+prefix = '/opt/ml/'
+model_path = os.path.join(prefix, 'model')
+
+# A singleton for holding the model. This simply loads the model and holds it.
+# It has a predict function that does a prediction based on the model and the input data.
+
+class ScoringService(object):
+    model = None                # Where we keep the model when it's loaded
+
+    @classmethod
+    def get_model(cls):
+        """Get the model object for this instance, loading it if it's not already loaded."""
+        if cls.model == None:
+            # TODO: replace with mxnet load
+            with open(os.path.join(model_path, 'decision-tree-model.pkl'), 'r') as inp:
+                cls.model = pickle.load(inp)
+        return cls.model
+
+    @classmethod
+    def predict(cls, input):
+        """For the input, do the predictions and return them.
+        Args:
+            input (a pandas dataframe): The data on which to do the predictions. There will be
+                one prediction per row in the dataframe"""
+        clf = cls.get_model()
+        return clf.predict(input)
+
+# The flask app for serving predictions
+app = flask.Flask(__name__)
+
+@app.route('/ping', methods=['GET'])
+def ping():
+    """Determine if the container is working and healthy. In this sample container, we declare
+    it healthy if we can load the model successfully."""
+    health = ScoringService.get_model() is not None  # You can insert a health check here
+
+    status = 200 if health else 404
+    return flask.Response(response='\n', status=status, mimetype='application/json')
+    
+@app.route('/invocations', methods=['POST'])
+def invocation():
+    """Do an inference on a single batch of data. In this sample server, we take data as CSV, convert
+    it to a pandas data frame for internal use and then convert the predictions back to CSV (which really
+    just means one prediction per line, since there's a single column.
+    """
+
+        # Convert from CSV to pandas
+    if flask.request.content_type == 'application/json':
+        body = app.current_request.json_body
+        if 'data' not in body:
+            raise BadRequestError('Missing image data')
+        image = base64.b64decode(body['data']) # byte array
+    else:
+        return flask.Response(response='This endpoint json data with image', status=415, mimetype='text/plain')
+
+    # Do the prediction
+    predictions = ScoringService.predict(image)
+
+    return flask.Response(response=predictions, status=200, mimetype='application/json')
